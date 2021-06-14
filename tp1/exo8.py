517d937bc228071b76d257bc0f71aca6b18bc21a
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-14 20:58:44 +0100

    fixing exo8 in tp1

--- tp1/exo8.py
+++ tp1/exo8.py
@@ -1,12 +1,14 @@
-n = int(input("Entrez un nombre de 3 chiffres: "))
+n = input("Entrez un nombre de 3 chiffres: ")
 
-while len(str(n)) != 3 or n == 0: 
-    n = int(input("Recommencez: "))
-
-n = str(n)
 c = n[0]
 d = n[1]
 u = n[2]
+
+while len(n) != 3 or int(c) == 0 or int(d) == 0 or int(u) == 0: 
+    c = n[0]
+    d = n[1]
+    u = n[2]
+    n = input("Recommencez: ")
 
 print(
     f'Les combinaisons possibles sont: {c+d+u}, {c+u+d}, {d+u+c}, {d+c+u}, {u+d+c}, {u+c+d} \n',
