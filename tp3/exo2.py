791181f2680d62f87597b33e851c650a59b7d20a
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-14 22:06:25 +0100

    correction exo2

--- tp3/exo2.py
+++ tp3/exo2.py
@@ -1,21 +1,24 @@
+#functions
 def decimal_to_binary(n):
     t = []
     while (n // 2 != 0):
         r = n % 2 
         t.append(r)
         n //= 2
-    if (n // 2 == 0):
-        t.append(n % 2)
+    t.append(n % 2)
     t.reverse()
     result = ''
     for el in t:
         result += str(el)
     return result
 
+#inputs
 n = int(input('Entrez un entier naturel: '))
 
+#guards
 while (n < 0):
     n = int(input('Recommencez: '))
 
+#execution
 print(decimal_to_binary(n))
 
