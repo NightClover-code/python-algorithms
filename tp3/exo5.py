320b3bc21ae6190422f86af7b9acfdbbfde34439
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-14 22:48:49 +0100

    correcting exo5.py

--- tp3/exo5.py
+++ tp3/exo5.py
@@ -1,19 +1,17 @@
+#functions
 def isConsecutive(t, n):
     isTrue = False
-    for i in range(0, n - 1):
+    for i in range(n - 1):
         if t[i] + 1 == t[i + 1]:
             isTrue = True 
         else:
             isTrue = False
     return isTrue
     
-
+#inputs
 n = int(input("Entrez le nombre d'élements: "))
 
-elements = []
-
-for i in range(0, n):
-    ele = int(input(f'Entrez la valeur {i}: '))
-    elements.append(ele)
+#execution
+elements = [int(input(f'Entrez la valeur {i}: ')) for i in range(n)]
 
 print(isConsecutive(elements, n))
