21bae1b5225f7b138af8a8d1070b27422d557e64
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-14 22:17:58 +0100

    refreshed exo8.py tp1

--- tp1/exo8.py
+++ tp1/exo8.py
@@ -1,14 +1,14 @@
 n = input("Entrez un nombre de 3 chiffres: ")
 
-c = n[0]
-d = n[1]
-u = n[2]
-
 while len(n) != 3 or int(c) == 0 or int(d) == 0 or int(u) == 0: 
     c = n[0]
     d = n[1]
     u = n[2]
     n = input("Recommencez: ")
+
+c = n[0]
+d = n[1]
+u = n[2]
 
 print(
     f'Les combinaisons possibles sont: {c+d+u}, {c+u+d}, {d+u+c}, {d+c+u}, {u+d+c}, {u+c+d} \n',
--- tp3/exo4.py
+++ tp3/exo4.py
@@ -1,3 +1,4 @@
+#functions
 def pypart(n):  
     T = [[0] * (n+1) for p in range(n+1)]
     for n in range(n+1):
@@ -11,12 +12,14 @@ def pypart(n):
                     T[n][k] = T[n-1][k-1] + T[n-1][k]
     return T
 
+#inputs
 n = int(input('Entrez un entier naturel n: '))
 
+#executions
 print(f'Triangle de pascal formé de {n}: ')
 
 for i in pypart(n):     
     for j in i:         
         if (j != 0):
             print(j, end="")      
-    print("\r")
+    print('\r')
