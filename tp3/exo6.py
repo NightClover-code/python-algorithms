89db3d7fdfee7c621007b55ccf99b690bd1343cb
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2021-06-14 22:55:50 +0100

    solving exo6

--- tp3/exo6.py
+++ tp3/exo6.py
@@ -1,20 +1,22 @@
+#functions
 def transpose(mat, tr, N):
     for i in range(N):
         for j in range(N):
             tr[i][j] = mat[j][i]
   
 def isSymmetric(mat, N):
-    tr = [ [0 for j in range(len(mat[0])) ] for i in range(len(mat)) ]
+    tr = [[0 for j in range(N)] for i in range(N) ]
     transpose(mat, tr, N)
     for i in range(N):
         for j in range(N):
             if (mat[i][j] != tr[i][j]):
                 return False
-    return True
+    return tr
 
-
+#inputs
 n = int(input("Entrez l'ordre de la matrice: "))
 
+#execution
 matrix= [[input(f"Entrez l'élement [{i}][{j}]") for j in range(n)] for i in range(n)]
 
 if isSymmetric(matrix, n) == True:
